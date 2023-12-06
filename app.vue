62b1cfb7e295c263ced22eec3bce886266d24763
ks960037 <ks960037@gmail.com>
2023-12-07 01:17:55 +0800

    新增導出功能,切換輸入模式

--- app.vue
+++ app.vue
@@ -6,22 +6,48 @@
         <h1>電話號碼分析工具</h1>
       </div>
     </div>
-    <!-- 選取檔案上傳的輸入框，當檔案改變時調用 onFileChange 方法 -->
+    <!-- 切換模式的 radio button -->
     <div class="row">
       <div class="col">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="mode" id="uploadMode" value="upload" v-model="mode">
+          <label class="form-check-label" for="uploadMode">
+            檔案上傳模式
+          </label>
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="mode" id="inputMode" value="input" v-model="mode">
+          <label class="form-check-label" for="inputMode">
+            文字輸入模式
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="row" v-if="mode === 'upload'">
+      <div class="col">
+        <!-- 選取檔案上傳的輸入框，當檔案改變時調用 onFileChange 方法 -->
         <input type="file" @change="onFileChange" class="form-control">
       </div>
     </div>
-    <!-- 確認按鈕，當點擊時將 showContent 設置為 true -->
+    <div class="row" v-if="mode === 'input'">
+      <div class="col">
+        <!-- 文字輸入模式 -->
+        <textarea class="form-control" v-model="fileContent"></textarea>
+      </div>
+    </div>
+    <!-- 確認按鈕，當點擊時處理資料 -->
     <div class="row">
       <div class="col">
-        <button @click="showContent = true" class="btn btn-primary mt-3">確認</button>
+        <button :disabled="!checkFileContent" @click="processNumbers" class="btn btn-primary mt-3 mx-3">確認</button>
+        <button :disabled="!checkContentProcessed" @click="downloadCSV" class="btn btn-primary mt-3 mx-3">下載 CSV</button>
       </div>
     </div>
     <!-- 輸出內容 -->
     <div class="row">
       <div class="col" v-for="(numbers, category) in processedContent" :key="category">
-        <div v-if="showContent && numbers.length > 0" class="card mt-3">
+        <div class="card mt-3">
           <div class="card-header">{{ category }}</div>
           <div class="card-body">
             <div v-for="(number, index) in numbers" :key="index">{{ number }}</div>
@@ -42,18 +68,20 @@ import { parsePhoneNumberFromString } from 'libphonenumber-js'
 export default {
   data() {
     return {
-      // 儲存檔案內容的數據屬性
-      fileContent: '',
-      processedContent: {
-        SMART: [],
-        GLOBE: [],
-        DITO: [],
-        CHERRY: [],
-        NONE: []
-      },
-      // 控制是否顯示檔案內容的數據屬性
-      showContent: false
+      mode: 'upload',  // 預設模式為檔案上傳模式
+      fileContent: '', // 儲存檔案內容的數據屬性
+      processedContent: null
     };
+  },
+  computed: {
+    // 計算屬性，判斷 fileContent 是否超過 1 個字元
+    checkFileContent() {
+      return this.fileContent.length > 1;
+    },
+    // 判斷檔案處理好了沒
+    checkContentProcessed() {
+      return this.processedContent != null;
+    }
   },
   methods: {
     getClassifyByNumber(input) {
@@ -153,8 +181,6 @@ export default {
       reader.onload = (e) => {
         // 將檔案的內容儲存到 fileContent 數據屬性中
         this.fileContent = e.target.result;
-        // 將檔案內容進行分析
-        this.processNumbers();
       };
       // 讀取檔案的內容
       reader.readAsText(file);
@@ -164,8 +190,13 @@ export default {
      * @return {void}
      */
     processNumbers() {
+      this.processedContent = {};
       // 將檔案內容分割成行
-      const lines = this.fileContent.split('\r\n');
+      let lines;
+      if (this.mode == "upload")
+        lines = this.fileContent.split('\r\n');
+      else
+        lines = this.fileContent.split('\n');
       // 使用 Set 來去除重複的行，然後再轉換回陣列
       const uniqueLines = [...new Set(lines)];
       // 對每一行進行處理
@@ -176,14 +207,38 @@ export default {
         if (phoneNumber && phoneNumber.isValid()) {
           // 分類
           const classify = this.getClassifyByNumber(phoneNumber.formatInternational());
-          this.processedContent[classify].push(`${phoneNumber.formatInternational()}`);
+          if (!this.processedContent[classify]) this.processedContent[classify] = [];
+          var formatNumber = phoneNumber.number.slice(1); // 去掉 + 號
+          this.processedContent[classify].push(formatNumber);
         } else {
+          if (!this.processedContent["NONE"]) this.processedContent["NONE"] = [];
           this.processedContent["NONE"].push('Invalid number')
         }
       });
-
-      // 將 showContent 設置為 true，以便在模板中顯示處理後的內容
-      this.showContent = true;
+    },
+    // 報表導出功能
+    downloadCSV() {
+      // 將處理後的內容轉換為 CSV 格式
+      let csvContent = '分類,電話號碼\n';
+      for (let category in this.processedContent) {
+        for (let number of this.processedContent[category]) {
+          csvContent += `${category},${number}\n`;
+        }
+      }
+      // 創建一個 Blob 對象來儲存 CSV 內容
+      const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+      // 創建一個可以下載的 URL
+      const url = URL.createObjectURL(blob);
+      // 創建一個隱藏的下載鏈接
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = '電話號碼分析結果.csv';
+      link.style.display = 'none';
+      document.body.appendChild(link);
+      // 觸發下載
+      link.click();
+      // 移除下載鏈接
+      document.body.removeChild(link);
     }
   }
 };
